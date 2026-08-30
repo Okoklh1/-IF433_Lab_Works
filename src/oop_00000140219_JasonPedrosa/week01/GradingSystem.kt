5592926d5eb82fc5a28cf2c5dbf909156b0022a1
Jason Pedrosa <jasonpedrosa97@gmail.com>
2026-08-30 15:50:58 +0700

    week01: (tugas 1) refactor variables to val and use string

--- src/oop_00000140219_JasonPedrosa/week01/GradingSystem.kt
+++ src/oop_00000140219_JasonPedrosa/week01/GradingSystem.kt
@@ -15,7 +15,7 @@ fun main() {
     println("Grade kamu: $grade")
     println("Status: ${calculateStatus(score)}")
 
-    val studentId: String = null
+    val studentId: String? = null
     val idLength = studentId?.length ?: 0
     println("Panjang studentId: $idLength")
 }
