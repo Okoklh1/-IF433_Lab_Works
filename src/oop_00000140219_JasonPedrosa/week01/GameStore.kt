1acb352278a1cb7fb37669523cb57a8c6781467f
Jason Pedrosa <jasonpedrosa97@gmail.com>
2026-08-30 16:06:06 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000140219_JasonPedrosa/week01/GameStore.kt
+++ src/oop_00000140219_JasonPedrosa/week01/GameStore.kt
@@ -3,4 +3,12 @@ package oop_00000140219_JasonPedrosa.week01
 fun main() {
     val gameTitle = "Super Mario"
     val price = 500000
+
+
 }
+
+fun calculateDiscount(price: Int) =
+    if (price < 100000) 0
+    else if (price <= 250000) price * 10 / 100
+    else if (price <= 500000) price * 20 / 100
+    else price * 30 / 100
