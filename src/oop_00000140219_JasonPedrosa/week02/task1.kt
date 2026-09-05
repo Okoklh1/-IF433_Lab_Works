315494befba4a7d363b5267b8467f77c4a7853a1
Jason Pedrosa <jasonpedrosa97@gmail.com>
2026-09-05 18:13:01 +0700

    week02: (task 1) implement library input system

--- src/oop_00000140219_JasonPedrosa/week02/task1.kt
+++ src/oop_00000140219_JasonPedrosa/week02/task1.kt
@@ -1,5 +1,7 @@
 package oop_00000140219_JasonPedrosa.week02
 
+import java.util.Scanner
+
 class Loan(
     val bookTitle: String,
     val borrower: String,
@@ -15,6 +17,28 @@ class Loan(
 }
 
     fun main(){
+        val scanner = Scanner(System.`in`)
 
+        println("--- LIBRARY FINE SYSTEM ---")
+
+        print("Masukkan Judul Buku: ")
+        val bookTitle = scanner.nextLine()
+
+        print("Masukkan Nama Peminjam: ")
+        val borrower = scanner.nextLine()
+
+        print("Masukkan Lama Pinjam (hari): ")
+        var loanDuration = scanner.nextInt()
+
+        if (loanDuration < 0) {
+            loanDuration = 1
+        }
+
+        val loan = Loan(bookTitle, borrower, loanDuration)
+
+        println("\n--- DETAIL PEMINJAMAN ---")
+        println("Judul Buku: ${loan.bookTitle}")
+        println("Peminjam: ${loan.borrower}")
+        println("Lama Pinjam: ${loan.loanDuration} hari")
+        println("Total Denda: Rp ${loan.calculateFine()}")
     }
-}
