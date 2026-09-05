41f9b1b194bc1f62a3d90a9228bce8a31c019122
Jason Pedrosa <jasonpedrosa97@gmail.com>
2026-09-05 17:31:23 +0700

    week02: add secondary constructor

--- src/oop_00000140219_JasonPedrosa/week02/Student.kt
+++ src/oop_00000140219_JasonPedrosa/week02/Student.kt
@@ -1,6 +1,10 @@
 package oop_00000140219_JasonPedrosa.week02
 
 class student(val name: String, val nim: String, var major: String){
+    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated"){
+        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
+    }
+
     init {
         if (nim.length != 5){
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
