dda78480d9c52995cbfd69b7042d9e6fc8090163
Jason Pedrosa <jasonpedrosa97@gmail.com>
2026-09-14 17:09:31 +0700

    week03: create basic Employee class

--- src/oop_00000140219_JasonPedrosa/week03/Employee.kt
+++ src/oop_00000140219_JasonPedrosa/week03/Employee.kt
@@ -1,22 +1,5 @@
 package oop_00000140219_JasonPedrosa.week03
 
-class employee(val name: String){
-    var salary : Int = 0
-        set(value) {
-            if (value < 0) {
-                println("ERROR: Gaji tidak boleh negatif! Di-set ke 0.")
-                field = 0
-            } else {
-                field = value
-            }
-        }
-    private var performanceRating: Int = 3
-    fun increasePerformance(){
-        performanceRating++
-        println("Kinerja $name meningkat! Rating: $performanceRating")
-    }
-
-    fun printStatus(){
-        println("Karyawan: $name, Rating: $performanceRating")
-    }
+class employee(val name: String) {
+    var salary: Int = 0
 }
--- src/oop_00000140219_JasonPedrosa/week03/Main.kt
+++
@@ -1,7 +0,0 @@
-package oop_00000140219_JasonPedrosa.week03
-
-fun main(){
-    val e = employee("Budi")
-    println("---Test Error---")
-    e.salary = -5000
-}
