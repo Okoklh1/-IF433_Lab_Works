a47c4d44b072faf9603a243fd9be241590f5fca7
Jason Pedrosa <jasonpedrosa97@gmail.com>
2026-09-14 17:58:55 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000140219_JasonPedrosa/week03/Weapon.kt
@@ -0,0 +1,23 @@
+package oop_00000140219_JasonPedrosa.week03
+
+class weapon(val name: String) {
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("WARNING: Damage tidak boleh negatif!")
+            } else if (value > 1000) {
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = if (damage > 800) {
+            "Legendary"
+        } else if (damage > 500) {
+            "Epic"
+        } else {
+            "Common"
+        }
+}
