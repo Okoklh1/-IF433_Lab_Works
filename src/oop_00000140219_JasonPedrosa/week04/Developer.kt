ecea6e1ddce9dceebbaab8853874ca792340775f
unknown <jasonpedrosa97@gmail.com>
2026-09-19 11:13:54 +0700

    week04: (task 2) create Developer class

---
+++ src/oop_00000140219_JasonPedrosa/week04/Developer.kt
@@ -0,0 +1,12 @@
+package oop_00000140219_JasonPedrosa.week04
+
+class developer(
+    name: String,
+    baseSalary: Int,
+    val programmingLanguage: String
+) : employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang ngoding menggunakan $programmingLanguage.")
+    }
+}
