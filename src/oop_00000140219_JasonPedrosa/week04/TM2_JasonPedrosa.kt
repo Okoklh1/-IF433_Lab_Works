8b3dd0564df70783e1058fecba52533bb1cd4558
unknown <jasonpedrosa97@gmail.com>
2026-09-19 11:17:40 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_00000140219_JasonPedrosa/week04/TM2_JasonPedrosa.kt
+++ src/oop_00000140219_JasonPedrosa/week04/TM2_JasonPedrosa.kt
@@ -1,2 +1,13 @@
 package oop_00000140219_JasonPedrosa.week04
 
+fun main() {
+    println("\n--- Testing Manager ---")
+    val manager = manager("Budi", 10000000)
+    manager.work()
+    println("Bonus Manager: Rp ${manager.calculateBonus()}")
+
+    println("\n--- Testing Developer ---")
+    val developer = developer("Andi", 8000000, "Python")
+    developer.work()
+    println("Bonus Developer: Rp ${developer.calculateBonus()}")
+}
