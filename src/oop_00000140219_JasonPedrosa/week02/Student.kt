b6c064c5d418062501c659e1951cd83a7afda982
unknown <jasonpedrosa97@gmail.com>
2026-09-19 10:28:29 +0700

    week04: create basic Vehicle parent class

--- src/oop_00000140219_JasonPedrosa/week02/Student.kt
+++ src/oop_00000140219_JasonPedrosa/week02/Student.kt
@@ -1,21 +1,7 @@
 package oop_00000140219_JasonPedrosa.week02
 
-class student(
     val name: String,
     val nim: String,
-    var major: String,
-    var gpa: Double =  0.0
-    ){
-    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated"){
-        println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
-    }
+){
 
-    init {
-        if (nim.length != 5){
-            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
-            println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
-        } else {
-            println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
-        }
-    }
 }
