840231961377111d60efc407cf88e5f1f4bf41cc
unknown <jasonpedrosa97@gmail.com>
2026-09-26 10:49:55 +0700

    week05: (task 1) create MathHelper class with overloading

--- src/oop_00000140219_JasonPedrosa/week05/MathHelper.kt
+++ src/oop_00000140219_JasonPedrosa/week05/MathHelper.kt
@@ -1,2 +1,16 @@
 package oop_00000140219_JasonPedrosa.week05
 
+class mathHelper {
+
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
